f3ce0443d924ad409f74489c6a2a4db216ed6903
crazedvic <victor@rubba.com>
2021-05-17 09:16:08 -0600

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,4 @@
+<div class="text-blue-900 bg-gray-200 w-full text-center p-6">MSP Directory</div>
+<div class="h-auto p-5 text-center border text-sm leading-4">This is the future home of MSP Directory<br>
+</div>
+<div class="border border-gray-200 fixed bottom-0 text-center w-full bg-gray-300">Copyright 2021 MSP Directory</div>
