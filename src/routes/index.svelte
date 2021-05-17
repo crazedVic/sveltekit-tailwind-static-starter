ac1bc8c8912ac41381457d80596a075212cdb20e
crazedvic <victor@rubba.com>
2021-05-17 10:06:10 -0600

    added bootstrap icons

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,3 +2,7 @@
 <div class="h-auto p-5 text-center border text-sm leading-4">This is the future home of MSP Directory<br>
 </div>
 <div class="border border-gray-200 fixed bottom-0 text-center w-full bg-gray-300">Copyright 2021 MSP Directory</div>
+<Facebook class="text-blue-800" width="32" height="32" />
+<script>
+    import Facebook from "svelte-bootstrap-icons/lib/Facebook";
+</script>
